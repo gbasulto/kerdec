1ab717118e83fd9cd536139bcd8b64078807d308
Guillermo Basulto-Elias <basulto@iastate.edu>
2016-02-14 18:19:16 -0600

    Add real part of empirical characteristi function in C++. It is already documented.

---
+++ src/ecf_real.cpp
@@ -0,0 +1,41 @@
+
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+
+using namespace arma;
+
+/* We will compute the real and imaginary parts of the empirical
+   characteristic function as well as its modulus and the empirical
+   characteristic function itself. All of this for univariate and
+   multivariate cases.
+
+   The purpose of doing it separately it is just for avoiding
+   unnecesary computations or use of complex numbers.
+ */
+
+/* --------------------------------------------------------------- */
+/* -------- Real part of ecf ------------------------------------- */
+/* --------------------------------------------------------------- */
+
+//' Real part of empirical characteristic function
+//'
+//' Real part of empirical characteristic function of a d-dimensional
+//' random variable. This function is evaluated at m vectors of size
+//' d.
+//'
+//' This function must receive matrices. Vectors or values are not
+//' accepted.
+//' 
+//' @param t mxd matrix where the function will be evaluated.
+//' @param smp nxd matrix with sample size if size n.
+//'
+//' @return A vector of size m with the real part of the empirical
+//' characteristic function.
+//'
+//' @export
+//[[Rcpp::export]]
+arma::vec ecf_re(arma::mat t, arma::mat smp)
+{
+  return mean(cos(t * trans(smp)), 1);
+}
+
